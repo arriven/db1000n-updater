19a7ba039246fe178b36a15a589c84c0d582b5af
arriven <20084245+Arriven@users.noreply.github.com>
2022-05-23 20:28:42 +0300

    add vendored openssl dependency

--- default.nix
+++ default.nix
@@ -2,9 +2,4 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "dev-environment";
-    buildInputs = [ openssl ];
-    shellHook = ''
-        export OPENSSL_DIR="${openssl.dev}"
-        export OPENSSL_LIB_DIR="${openssl.out}/lib"
-    '';
 }
